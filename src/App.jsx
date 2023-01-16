34ce351c0dc5c9132f5f0cf5ae791ad083e9ae72
satyasai2108 <satyasai2108@gmail.com>
2023-01-16 13:34:45 +0530

    Revert "Revert "Revert "changed pages"""
    This reverts commit c0bbdbbf6444e3ec5e3d786e0a9cd7ebaa0ea950.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import "./css/style.css";
 
 import AOS from "aos";
 
-import Home from "./src/pages/Home";
+import Home from "./pages/Home";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
 import ResetPassword from "./pages/ResetPassword";
